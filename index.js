3bb1968ea750429a50737c4b9fe839a5082f859b
yueshen <qi1071921724@gmail.com>
2022-10-11 14:57:21 +0800

    全部修改提交

---
+++ index.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    var div = document.querySelector('div');
+    div.addEventListener('click', function() {
+        div.style.backgroundColor = '#fff';
+    })
+})
