2276064813011dce46ed52baf162f6020145e6ed
yueshen <qi1071921724@gmail.com>
2022-10-11 15:14:32 +0800

    移除了index.js和index.css

--- index.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function() {
-    var div = document.querySelector('div');
-    div.addEventListener('click', function() {
-        div.style.backgroundColor = '#fff';
-    })
-})
